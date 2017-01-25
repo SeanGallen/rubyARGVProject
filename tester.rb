555cb029967a89952b4a990f44e7e40f77e6e5f8
Sean <sjgallen@gmail.com>
2017-01-25 00:14:50 -0600

    created a class to work with input

---
+++ tester.rb
@@ -0,0 +1,12 @@
+class Tester
+tester = gets.chomp
+while (tester != "q" )
+  tester = gets.chomp
+  if tester.to_f.to_s == tester.to_s || tester.to_i.to_s == tester.to_s
+     puts "test this number"
+  else
+    puts "test the letter"
+  end
+end
+puts "End of Test"
+end
